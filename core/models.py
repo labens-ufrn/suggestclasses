bc06f40f3cdaaee24881260407d1f54df546bc76
Taciano Morais Silva <tacianosilva@gmail.com>
2019-05-09 22:19:07 -0300

    Modificações nos modelos e criação de um primeiro teste de unidade.

--- core/models.py
+++ core/models.py
@@ -3,14 +3,40 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
+
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     instance.profile.save()
+
+
+class ComponenteCurricular(models.Model):
+    '''
+        Um componente curricular tem código, nome, ementa, departamento, carga horária, equivalências, requisitos, data de criação.
+    '''
+    codigo = models.IntegerField()
+    nome = models.CharField(max_length=200)
+    ementa = models.CharField(max_length=200)
+    departamento = models.CharField(max_length=200)
+    carga_horaria = models.IntegerField()
+
+
+class EstruturaCurricular(models.Model):
+    codigo = models.IntegerField()
+    sigla = models.CharField(max_length=10)
+    nome = models.CharField(max_length=200)
+    ano_periodo = models.CharField(max_length=10)
+
+class OrganizacaoCurricular(models.Model):
+    estrutura = models.ForeignKey(EstruturaCurricular, on_delete=models.PROTECT)
+    componente = models.ForeignKey(ComponenteCurricular, on_delete=models.PROTECT)
+    periodo = models.IntegerField()
+    obrigatoria = models.BooleanField()
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
+import django
+django.setup()
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import EstruturaCurricular
+
+class EstruturaCurricularTests(TestCase):
+
+    def test_create_estrutura(self):
+        """
+                Teste de Unidade em Python.
+        """
+        bsi = EstruturaCurricular(sigla = '01A', ano_periodo = '2011.1', nome = 'SISTEMAS DE INFORMAÇÃO - Presencial - MT')
+
+        self.assertEquals('01A',bsi.sigla)
