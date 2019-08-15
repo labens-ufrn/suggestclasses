3b6023ab0d8626caf5f9c98b71f1edf657699374
Taciano Morais Silva <tacianosilva@gmail.com>
2019-08-15 12:16:05 -0300

    Adicionando Modelos para Centro e Sala.

--- core/models.py
+++ core/models.py
@@ -3,12 +3,34 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Centro(models.Model):
+    """
+        Um centro tem código, nome, sigla, endereço e site.
+    """
+    codigo = models.IntegerField()
+    nome = models.CharField(max_length=200, unique=True)
+    sigla = models.CharField(max_length=10, unique=True)
+    endereco = models.CharField(max_length=250, blank=True, null=True)
+    site = models.CharField(max_length=250)
+
+
+class Sala(models.Model):
+    """
+        Uma sala tem um número, um nome, capacidade, tamanho, bloco.
+    """
+    nome = models.CharField(max_length=200, blank=True, null=True)
+    sigla = models.CharField(max_length=10)
+    capacidade = models.IntegerField()
+    tamanho = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    bloco = models.CharField(max_length=10)
+    estrutura = models.ForeignKey(Centro, on_delete=models.PROTECT)
+
 
 class ComponenteCurricular(models.Model):
     """
         Um componente curricular tem código, nome, ementa, departamento, carga horária, equivalências, requisitos, data de criação.
     """
-    codigo = models.IntegerField()
+    codigo = models.IntegerField(unique=True)
     nome = models.CharField(max_length=200)
     ementa = models.CharField(max_length=200)
     departamento = models.CharField(max_length=200)
@@ -16,16 +38,18 @@ class ComponenteCurricular(models.Model):
 
 
 class EstruturaCurricular(models.Model):
-    codigo = models.IntegerField()
-    sigla = models.CharField(max_length=10)
+    codigo = models.IntegerField(unique=True)
+    sigla = models.CharField(max_length=10, unique=True)
     nome = models.CharField(max_length=200)
     ano_periodo = models.CharField(max_length=10)
+
 
 class OrganizacaoCurricular(models.Model):
     estrutura = models.ForeignKey(EstruturaCurricular, on_delete=models.PROTECT)
     componente = models.ForeignKey(ComponenteCurricular, on_delete=models.PROTECT)
     periodo = models.IntegerField()
     obrigatoria = models.BooleanField()
+
 
 class Horario(models.Model):
     ORDENS = (
@@ -36,6 +60,7 @@ class Horario(models.Model):
         ('5', 'Quinto Horário'),
         ('6', 'Sexto Horário'),
     )
+
     TURNOS = (
         ('M', 'Manhã'),
         ('T', 'Tarde'),
