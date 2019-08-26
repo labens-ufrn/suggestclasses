ad5ff08d4698c6655fb834e7fefd62be95bb3f00
Taciano Morais Silva <tacianosilva@gmail.com>
2019-08-26 00:32:31 -0300

    Adicionando Modelos para Departamento, Curso e Componente Curricular.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Horario
+from .models import Horario, Centro, Departamento, Curso, ComponenteCurricular
 
+admin.site.register(Centro)
+admin.site.register(Departamento)
+admin.site.register(ComponenteCurricular)
+admin.site.register(Curso)
 admin.site.register(Horario)
--- core/models.py
+++ core/models.py
@@ -3,15 +3,47 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Centro(models.Model):
     """
         Um centro tem código, nome, sigla, endereço e site.
     """
-    codigo = models.IntegerField()
     nome = models.CharField(max_length=200, unique=True)
     sigla = models.CharField(max_length=10, unique=True)
     endereco = models.CharField(max_length=250, blank=True, null=True)
     site = models.CharField(max_length=250)
+
+    def __str__(self):
+        return  self.sigla
+
+
+class Departamento(models.Model):
+    """
+        Um departamento tem código, nome, sigla, endereço e site.
+    """
+    nome = models.CharField(max_length=200, unique=True)
+    sigla = models.CharField(max_length=10, unique=True)
+    endereco = models.CharField(max_length=250, blank=True, null=True)
+    site = models.CharField(max_length=250)
+    centro = models.ForeignKey(Centro, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return  self.sigla
+
+
+class Curso(models.Model):
+        """
+            Um curso tem: código, nome, habilitação, turnos, modalidade.
+        """
+        codigo = models.IntegerField(unique=True)
+        nome = models.CharField(max_length=200)
+        turno = models.CharField(max_length=3)
+        habilitacao = models.CharField(max_length=250)
+        modalidade = models.CharField(max_length=250)
+        centro = models.ForeignKey(Centro, on_delete=models.PROTECT)
+
+        def __str__(self):
+            return self.nome + ' - ' + self.centro.sigla
 
 
 class Sala(models.Model):
@@ -23,18 +55,26 @@ class Sala(models.Model):
     capacidade = models.IntegerField()
     tamanho = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     bloco = models.CharField(max_length=10)
-    estrutura = models.ForeignKey(Centro, on_delete=models.PROTECT)
+    centro = models.ForeignKey(Centro, on_delete=models.PROTECT)
 
 
 class ComponenteCurricular(models.Model):
     """
         Um componente curricular tem código, nome, ementa, departamento, carga horária, equivalências, requisitos, data de criação.
     """
-    codigo = models.IntegerField(unique=True)
+    codigo = models.CharField(max_length=10)
     nome = models.CharField(max_length=200)
-    ementa = models.CharField(max_length=200)
-    departamento = models.CharField(max_length=200)
-    carga_horaria = models.IntegerField()
+    ementa = models.TextField(max_length=500)
+    ch_total = models.IntegerField()
+    ch_teorica = models.IntegerField()
+    ch_pratica = models.IntegerField()
+    requisito = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True, related_name='pre_requisitos')
+    corequisito = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True, related_name='co_requisitos')
+    equivalencia = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True, related_name='equivalencias')
+    departamento = models.ForeignKey(Departamento, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.codigo + ' - ' + self.nome
 
 
 class EstruturaCurricular(models.Model):
@@ -52,6 +92,16 @@ class OrganizacaoCurricular(models.Model):
 
 
 class Horario(models.Model):
+    DIAS = (
+        ('1', 'Domingo'),
+        ('2', 'Segunda'),
+        ('3', 'Terça'),
+        ('4', 'Quarta'),
+        ('5', 'Quinta'),
+        ('6', 'Sexta'),
+        ('7', 'Sábado'),
+    )
+
     ORDENS = (
         ('1', 'Primeiro Horário'),
         ('2', 'Segundo Horário'),
@@ -66,13 +116,15 @@ class Horario(models.Model):
         ('T', 'Tarde'),
         ('N', 'Noite'),
     )
-    ordem = models.CharField(max_length=1, choices= ORDENS)
-    turno = models.CharField(max_length=10, choices= TURNOS)
+
+    dia = models.CharField(max_length=1, choices=DIAS)
+    turno = models.CharField(max_length=10, choices=TURNOS)
+    ordem = models.CharField(max_length=1, choices=ORDENS)
     hora_inicio = models.TimeField()
-    hora_fim = models.TimeField()
+    hora_final = models.TimeField()
 
     class Meta:
-        unique_together = ("ordem", "turno")
+        unique_together = ("dia", "turno", "ordem")
 
     def __str__(self):
-        return self.ordem + self.turno
+        return  self.dia + self.turno + self.ordem
