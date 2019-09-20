e5733212637d26ab57c9354309f7508e83c14150
Taciano Morais Silva <tacianosilva@gmail.com>
2019-09-20 16:36:20 -0300

    Modificações nas telas de Horários e no Index, utilizando templates e um layout base para as páginas.

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.list, name='list'),
     path('<int:horario_id>/', views.detail, name='detail'),
+    path('curso/<int:curso_id>/', views.curso_detail, name='curso_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,21 +1,53 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+import json, requests
 
+from core.models import Curso, Departamento
 from .models import Horario
 
+
 def index(request):
-    horario_list = Horario.objects.all()
+    cursos = Curso.objects.all()
+    departamentos = Departamento.objects.all()
+
     template = loader.get_template('core/index.html')
+
+    #response = requests.get("https://servicos.jfrn.jus.br/cartaapi/servicos")
+    #comments = json.loads(response.content)
+
     context = {
-        'horario_list': horario_list,
+        'departamentos': departamentos,
+        'cursos': cursos
     }
     return HttpResponse(template.render(context, request))
+
 
 def detail(request, horario_id):
     return HttpResponse("You're looking at Horario %s." % horario_id)
 
+
+def curso_detail(request, curso_id):
+    curso = Curso.objects.get(pk=curso_id)
+    return HttpResponse("You're looking at Curso %s." % curso)
+
+
 def list(request):
     horario_list = Horario.objects.all()
-    context = {'horario_list': horario_list}
+    horarios = []
+
+    for i in range(1, 7):
+        horarios.append(Horario.objects.filter(ordem=i, turno='M').order_by('dia'))
+
+    for i in range(1, 7):
+        horarios.append(Horario.objects.filter(ordem=i, turno='T').order_by('dia'))
+
+    for i in range(1, 5):
+        horarios.append(Horario.objects.filter(ordem=i, turno='N').order_by('dia'))
+
+    context = {
+        'horario_list': horario_list,
+        'horarios': horarios
+    }
+
     return render(request, 'core/list.html', context)
