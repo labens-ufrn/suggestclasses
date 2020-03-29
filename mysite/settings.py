9b560bdc999c7a3bcc986a3678fb25da5673ff2b
Taciano Morais Silva <tacianosilva@gmail.com>
2020-03-28 21:13:04 -0300

    Adicionando configurações para a pasta de arquivos chamada 'media'.

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,3 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
+
+# User_Uploaded_Files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
