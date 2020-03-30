816a32dbb9ebca49c31e5682402789e324aa016a
Taciano Morais Silva <tacianosilva@gmail.com>
2020-03-30 16:21:43 -0300

    Criação de função para retornar Componentes Curriculares dada uma Estrutura - issue #19.

--- core/bo/sevices.py
+++ core/bo/sevices.py
@@ -1,6 +1,6 @@
 from django.db.models import Sum
 
-from core.models import OrganizacaoCurricular
+from core.models import OrganizacaoCurricular, ComponenteCurricular
 
 
 def get_oc_by_semestre(estrutura, semestre):
@@ -10,3 +10,10 @@ def get_oc_by_semestre(estrutura, semestre):
 def get_ch_by_semestre(estrutura, semestre):
     return OrganizacaoCurricular.objects.filter(estrutura=estrutura, semestre=semestre)\
         .aggregate(Sum("componente__ch_total"))
+
+
+def get_cc_by_estrutura(estrutura):
+    oc = OrganizacaoCurricular.objects.filter(estrutura=estrutura).values_list('componente', flat=True)
+    loc = list(oc)
+    componentes = ComponenteCurricular.objects.filter(pk__in=loc)
+    return componentes
