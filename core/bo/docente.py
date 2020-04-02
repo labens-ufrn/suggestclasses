e03b36eaef6927b465f13b1e32d37d7a4c5cacb1
Taciano Morais Silva <tacianosilva@gmail.com>
2020-04-01 23:26:27 -0300

    Acrescentando a busca de docente pelo nome.
    A busca pelo docente foi realizada pelo nome do coordenador, pois no csv de curso não tem o siape do docente. Existe um id (id_servidor) que não encontramos em outros csv.

--- core/bo/docente.py
+++ core/bo/docente.py
@@ -7,3 +7,10 @@ def get_docente_by_siape(siape):
         # Professores Substitutos e Temporários não estão na lista
         docente = Docente.objects.get(siape=siape)
     return docente
+
+
+def get_docente_by_nome(nome):
+    docente = None
+    if nome != '' and Docente.objects.filter(nome=nome).exists():
+        docente = Docente.objects.get(nome=nome)
+    return docente
