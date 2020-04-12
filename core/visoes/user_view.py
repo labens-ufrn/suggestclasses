e25ef25794543ec2e77a6d2a31ec4ad9cfd63112
Taciano Morais Silva <tacianosilva@gmail.com>
2020-04-12 11:23:25 -0300

    Carregamento de Grupos de Usuário a partir da Função Gratificada #23.

--- core/visoes/user_view.py
+++ core/visoes/user_view.py
@@ -7,15 +7,13 @@ from django.core.exceptions import ValidationError
 
 from core.models import Docente, FuncaoGratificada
 
-grupos = []
-
 
 def criar_usuario(request, form_usuario):
     usuario = form_usuario.save(commit=False)
 
     # check_username(request, form_usuario)
     check_email(request, form_usuario)
-    check_grupos(request, form_usuario, usuario)
+    grupos = check_grupos(request, form_usuario, usuario)
 
     usuario.save()
     for g in grupos:
@@ -41,34 +39,52 @@ def check_email(request, form_usuario):
 def check_grupos(request, form_usuario, usuario):
     matricula = form_usuario.cleaned_data.get('matricula')
     grupo_selecionado = form_usuario.cleaned_data.get('grupo')
-
+    grupos = []
     if Docente.objects.filter(siape=matricula).exists():
-        docentes = 'Docentes'
-        if Group.objects.filter(name=docentes).exists():
-            grupo_docentes = Group.objects.get(name=docentes)
-            grupos.append(grupo_docentes)
-        # Verifica a existência da função gratificada ativa
+        grupos.append(get_grupo_docentes())
+
         hoje = date.today()
         if FuncaoGratificada.objects.filter(siape=matricula, inicio__lte=hoje, fim__gt=hoje).exists():
             fgs = FuncaoGratificada.objects.filter(siape=matricula, inicio__lte=hoje, fim__gt=hoje)
             for fg in fgs:
                 print(fg.atividade + " " + fg.atividade)
                 if fg.atividade == 'CHEFE DE DEPARTAMENTO':
-                    chefes = 'Chefes'
-                    grupo_docentes = Group.objects.get(name=chefes)
-                    grupos.append(grupo_docentes)
+                    grupos.append(get_grupo_chefes())
                 if fg.atividade == 'COORDENADOR DE CURSO':
-                    coordenadores = 'Coordenadores'
-                    grupo_docentes = Group.objects.get(name=coordenadores)
-                    grupos.append(grupo_docentes)
-        return
+                    grupos.append(get_grupo_coordenadores())
+        return grupos
     # TODO Criar model de Discente para essa checagem
     # elif Discente.objects.filter(matricula=matricula).exists():
     #      grupo_chefes = form_usuario.cleaned_data.get('Chefes')
     #      usuario.groups.add(grupo_chefes)
-
-    messages.error(request, 'A matrícula informada não está associada a um discente ou docente!')
+    msg = 'A matrícula informada não está associada a um discente ou docente!'
+    form_usuario.add_error('matricula', msg)
+    messages.error(request, msg)
     raise ValidationError("A matrícula não foi encontrada!")
+
+
+def get_grupo_docentes():
+    docentes = 'Docentes'
+    grupo_docentes = Group.objects.get(name=docentes)
+    return grupo_docentes
+
+
+def get_grupo_chefes():
+    chefes = 'Chefes'
+    grupo_chefes = Group.objects.get(name=chefes)
+    return grupo_chefes
+
+
+def get_grupo_coordenadores():
+    coordenadores = 'Coordenadores'
+    grupo_coordenadores = Group.objects.get(name=coordenadores)
+    return grupo_coordenadores
+
+
+def get_grupo_discentes():
+    discentes = 'Discentes'
+    grupo_discentes = Group.objects.get(name=discentes)
+    return grupo_discentes
 
 
 def autenticar_logar(request, form_usuario):
