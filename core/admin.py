7abc324b7635a293b2c00d76c1eb3c30640feb62
Taciano Morais Silva <tacianosilva@gmail.com>
2020-04-12 17:04:22 -0300

    Adicionando modelo Discente ao Admin App.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from core.models import Horario, Centro, Departamento, Curso, ComponenteCurricular, EstruturaCurricular, \
-    OrganizacaoCurricular, Docente, Turma, SugestaoTurma, Sala, FuncaoGratificada
+    OrganizacaoCurricular, Docente, Turma, SugestaoTurma, Sala, FuncaoGratificada, Discente
 
 admin.site.register(Centro)
 admin.site.register(Departamento)
@@ -15,3 +15,4 @@ admin.site.register(Turma)
 admin.site.register(SugestaoTurma)
 admin.site.register(Sala)
 admin.site.register(FuncaoGratificada)
+admin.site.register(Discente)
