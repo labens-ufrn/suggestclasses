6f5963ab47c32b1177254ed226d54fc184941093
Kmiokande <brunnodesouzabezerra@gmail.com>
2020-05-11 23:36:48 -0300

    Correção do atributo sexo em povoar_discentes.py

--- dados/povoar_discentes.py
+++ dados/povoar_discentes.py
@@ -26,6 +26,10 @@ def carregar_discentes(row):
 
         if not Discente.objects.filter(matricula=matricula).exists():
             print("Adicionando Discente " + matricula + " - " + nome_discente + "- " + nome_curso)
+            if sexo == "M" or sexo == "F":
+                sexo = sexo
+            else:
+                sexo = "N"
             discente = Discente(matricula=matricula, nome_discente=nome_discente, sexo=sexo,
                                 ano_ingresso=ano_ingresso, periodo_ingresso=periodo_ingresso,
                                 forma_ingresso=forma_ingresso, tipo_discente=tipo_discente, status=status,
