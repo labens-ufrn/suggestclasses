3a15a768b3369eaa3f157f83f2f5f66d7e3d49bf
Taciano Morais Silva <tacianosilva@gmail.com>
2020-05-18 10:57:50 -0300

    Script para a criação de Grupos e Permissões #75

--- dados/povoar_grupos.py
+++ dados/povoar_grupos.py
@@ -2,7 +2,7 @@ import django
 django.setup()
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
-from core.models import Horario, SugestaoTurma
+from core.models import Horario, SugestaoTurma, SolicitacaoTurma
 
 
 def main():
@@ -12,8 +12,10 @@ def main():
 
 def criar_grupos():
     docentes, created = Group.objects.get_or_create(name='Docentes')
+    adicionar_permissoes_docentes(docentes)
 
     discentes, created = Group.objects.get_or_create(name='Discentes')
+    adicionar_permissoes_discentes(discentes)
 
     chefes, created = Group.objects.get_or_create(name='Chefes')
     adicionar_permissoes_chefes(chefes)
@@ -29,10 +31,37 @@ def adicionar_permissoes_chefes(chefes):
     add_permissao(codename='add_sugestaoturma', content_type=ct, grupo=chefes)
     add_permissao(codename='delete_sugestaoturma', content_type=ct, grupo=chefes)
 
+    ct = ContentType.objects.get_for_model(SolicitacaoTurma)
+    add_permissao(codename='view_solicitacaoturma', content_type=ct, grupo=chefes)
+
 
 def adicionar_permissoes_coordenadores(coordenadores):
     ct = ContentType.objects.get_for_model(SugestaoTurma)
-    permission = Permission.objects.get(name="change_sugestaoturma")
+    add_permissao(codename='view_sugestaoturma', content_type=ct, grupo=coordenadores)
+    add_permissao(codename='change_sugestaoturma', content_type=ct, grupo=coordenadores)
+    add_permissao(codename='add_sugestaoturma', content_type=ct, grupo=coordenadores)
+
+    ct = ContentType.objects.get_for_model(SolicitacaoTurma)
+    add_permissao(codename='view_solicitacaoturma', content_type=ct, grupo=coordenadores)
+
+
+def adicionar_permissoes_docentes(docentes):
+    ct = ContentType.objects.get_for_model(SugestaoTurma)
+    add_permissao(codename='view_sugestaoturma', content_type=ct, grupo=docentes)
+
+    ct = ContentType.objects.get_for_model(SolicitacaoTurma)
+    add_permissao(codename='view_solicitacaoturma', content_type=ct, grupo=docentes)
+
+
+def adicionar_permissoes_discentes(discentes):
+    ct = ContentType.objects.get_for_model(SugestaoTurma)
+    add_permissao(codename='view_sugestaoturma', content_type=ct, grupo=discentes)
+
+    ct = ContentType.objects.get_for_model(SolicitacaoTurma)
+    add_permissao(codename='view_solicitacaoturma', content_type=ct, grupo=discentes)
+    add_permissao(codename='add_solicitacaoturma', content_type=ct, grupo=discentes)
+    add_permissao(codename='change_solicitacaoturma', content_type=ct, grupo=discentes)
+    add_permissao(codename='delete_solicitacaoturma', content_type=ct, grupo=discentes)
 
 
 def add_permissao(codename, content_type, grupo):
