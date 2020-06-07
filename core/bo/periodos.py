724bc549b62b66ec7b105b996effea6a2532f510
Taciano Morais Silva <tacianosilva@gmail.com>
2020-06-07 14:28:31 -0300

    Criação de BO para o model de Período Letivo #100

---
+++ core/bo/periodos.py
@@ -0,0 +1,18 @@
+from django.db.models import Q
+
+from core.models import PeriodoLetivo
+
+
+def get_periodo_letivo(status, ano=None, periodo=None):
+    query = Q(status=status)
+    if ano:
+        query.add(Q(ano=ano), Q.AND)
+    if periodo:
+        query.add(Q(periodo=periodo), Q.AND)
+    return PeriodoLetivo.objects.filter(query)
+
+
+def get_periodo_planejado():
+    periodos = get_periodo_letivo(status=PeriodoLetivo.PLANEJADO)
+    periodo_letivo = periodos.first()
+    return periodo_letivo
