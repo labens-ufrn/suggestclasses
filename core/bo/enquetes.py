3cb4d08e581c5a65a91cde9bf2793af3e9896cc1
Taciano Morais Silva <tacianosilva@gmail.com>
2020-11-13 17:30:03 -0300

    Ordenação por data na lista de enquetes #105

--- core/bo/enquetes.py
+++ core/bo/enquetes.py
@@ -23,7 +23,7 @@ def get_enquetes_por_curso(curso_id=None):
         Lista todos as Enquetes.
     """
     return Enquete.objects.filter(curso__codigo=curso_id)\
-            .order_by('curso', 'status', 'data_hora_inicio', 'nome', 'periodo')
+            .order_by('curso', 'status', '-data_hora_inicio', 'nome', 'periodo')
 
 
 def get_componentes_enquete(enquete):
