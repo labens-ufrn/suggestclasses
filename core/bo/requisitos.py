c2344afa3d64c9d0b447c5feef31eb48d8b73bdc
Taciano Morais Silva <tacianosilva@gmail.com>
2020-11-21 21:31:51 -0300

    Avaliação da expressão de requisitos #111
    Tranformamos em uma expressão boleana e avaliamos.

---
+++ core/bo/requisitos.py
@@ -0,0 +1,30 @@
+
+
+def create_token_lst(s):
+    """create token list:
+    """
+    s = s.replace('(', ' ( ')
+    s = s.replace(')', ' ) ')
+
+    return s.split()
+
+
+def replace_and(s):
+    s = s.replace('E', 'and')
+    return s
+
+def replace_or(s):
+    s = s.replace('OU', 'or')
+    return s
+
+def replace_expressao(s, disciplinas):
+    expressao = replace_and(s)
+    expressao = replace_or(expressao)
+    tokens = create_token_lst(expressao)
+    for t in tokens:
+        if t != '(' and t != ')' and t != 'and' and t != 'or':
+            if t in disciplinas:
+                expressao = expressao.replace(t, 'True')
+            else:
+                expressao = expressao.replace(t, 'False')
+    return expressao
