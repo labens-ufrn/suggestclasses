30d1cf5c03beb464a13944227156fab4c6237b4e
Taciano Morais Silva <tacianosilva@gmail.com>
2020-11-30 22:25:30 -0300

    Criação do form para Histórico #111

--- core/forms.py
+++ core/forms.py
@@ -3,10 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 
+from core.bo.curso import get_curso_by_codigo
+from core.bo.curriculo import get_componentes_by_curso, get_semestres_by_curso
 from core.bo.enquetes import get_componentes_enquete
 from core.bo.sevices import get_cc_by_estrutura, get_estrutura_by_curso
 from core.dao.componente_dao import get_componentes_curriculares
-from core.models import SugestaoTurma, Docente, ComponenteCurricular, Departamento, Sala, \
+from core.models import Historico, SugestaoTurma, Docente, ComponenteCurricular, Departamento, Sala, \
     VotoTurma
 
 
@@ -95,5 +97,22 @@ class VotoTurmaForm(ModelForm):
     def __init__(self, *args, **kwargs):
         enquete = kwargs.pop('enquete')
         super(VotoTurmaForm, self).__init__(*args, **kwargs)
-        get_componentes_curriculares()
         self.fields['componente'].queryset = get_componentes_enquete(enquete)
+
+
+class HistoricoForm(ModelForm):
+    semestre = forms.ModelChoiceField(queryset=None, label='Semestre do Curso')
+    componente = forms.ModelChoiceField(queryset=None, label='Componente Cursado')
+    curso = forms.CharField(label='Curso', widget=forms.HiddenInput, required=False)
+    class Meta:
+        model = Historico
+        fields = ['semestre', 'componente', 'curso']
+
+    def __init__(self, *args, **kwargs):
+        discente = kwargs.pop('discente')
+        super(HistoricoForm, self).__init__(*args, **kwargs)
+        self.fields['componente'].queryset = ComponenteCurricular.objects.none()
+        self.initial['curso'] = discente.id_curso
+        curso = get_curso_by_codigo(discente.id_curso) 
+        self.fields['semestre'].choices = get_semestres_by_curso(curso)
+        # self.fields['componente'].queryset = get_componentes_by_curso(curso)
