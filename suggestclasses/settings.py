aeb0f34332a92892a3889457451961c95faea299
Taciano Morais Silva <tacianosilva@gmail.com>
2021-04-04 00:01:29 -0300

    Add variável de ambiente para o HOST

--- suggestclasses/settings.py
+++ suggestclasses/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': '127.0.0.1',
+        'HOST': config('DATABASE_HOST'),
         'PORT': config('MARIADB_PORT'),
         # optional:
         'OPTIONS': {
