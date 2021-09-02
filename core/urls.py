adc97081c59f144f097cafdca37cd0c5b65a3615
Taciano Morais Silva <tacianosilva@gmail.com>
2021-09-01 21:22:04 -0300

    feat: add sugestões para geografia #84

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,9 @@ from django.urls import path
 from suggestclasses.settings import STATIC_URL, MEDIA_URL, MEDIA_ROOT, DEBUG
 from . import views
 from .visoes import suggest_view, enquete_view, profile_view
-from .visoes import sugestao_letras_view, sugestao_historia_view
+from .visoes import sugestao_letras_view
+from .visoes import sugestao_geografia_view
+from .visoes import sugestao_historia_view
 from .visoes.sugestao_contabeis_view import sugestao_cont_list, sugestao_cont_manter, sugestao_cont_incluir, \
     sugestao_cont_editar, sugestao_cont_deletar
 
@@ -133,6 +135,18 @@ urlpatterns = [
     path('sugestao/dir/incluir', views.sugestao_dir_incluir, name='sugestao_dir_incluir'),
     path('sugestao/dir/editar/<int:pk>/', views.sugestao_dir_editar, name='sugestao_dir_editar'),
     path('sugestao/dir/deletar/<int:pk>/', views.sugestao_dir_deletar, name='sugestao_dir_deletar'),
+
+    path('sugestao/geo-bac', sugestao_geografia_view.sugestao_geo_bac_list, name='sugestao_geo_bac_list'),
+    path('sugestao/geo-bac/manter', sugestao_geografia_view.sugestao_geo_bac_manter, name='sugestao_geo_bac_manter'),
+    path('sugestao/geo-bac/incluir', sugestao_geografia_view.sugestao_geo_bac_incluir, name='sugestao_geo_bac_incluir'),
+    path('sugestao/geo-bac/editar/<int:pk>/', sugestao_geografia_view.sugestao_geo_bac_editar, name='sugestao_geo_bac_editar'),
+    path('sugestao/geo-bac/deletar/<int:pk>/', sugestao_geografia_view.sugestao_geo_bac_deletar, name='sugestao_geo_bac_deletar'),
+
+    path('sugestao/geo-lic', sugestao_geografia_view.sugestao_geo_lic_list, name='sugestao_geo_lic_list'),
+    path('sugestao/geo-lic/manter', sugestao_geografia_view.sugestao_geo_lic_manter, name='sugestao_geo_lic_manter'),
+    path('sugestao/geo-lic/incluir', sugestao_geografia_view.sugestao_geo_lic_incluir, name='sugestao_geo_lic_incluir'),
+    path('sugestao/geo-lic/editar/<int:pk>/', sugestao_geografia_view.sugestao_geo_lic_editar, name='sugestao_geo_lic_editar'),
+    path('sugestao/geo-lic/deletar/<int:pk>/', sugestao_geografia_view.sugestao_geo_lic_deletar, name='sugestao_geo_lic_deletar'),
 
     path('sugestao/his-bac', sugestao_historia_view.sugestao_his_bac_list, name='sugestao_his_bac_list'),
     path('sugestao/his-bac/manter', sugestao_historia_view.sugestao_his_bac_manter, name='sugestao_his_bac_manter'),
---
+++ core/visoes/sugestao_geografia_view.py
@@ -0,0 +1,86 @@
+from django.contrib.auth.decorators import permission_required
+
+from core.bo.sevices import get_estrutura_geografia_bacharelado, get_estrutura_geografia_licenciatura
+from core.visoes.suggest_view import (sugestao_deletar, sugestao_editar, sugestao_grade_horarios, sugestao_incluir, sugestao_manter)
+
+
+def sugestao_geo_bac_list(request):
+    geo_bac_ec = get_estrutura_geografia_bacharelado()
+    sugestao_incluir_link = '/core/sugestao/geo-bac/incluir'
+    sugestao_manter_link = '/core/sugestao/geo-bac/manter'
+    sugestao_list_link = '/core/sugestao/geo-bac'
+    return sugestao_grade_horarios(request, geo_bac_ec, sugestao_incluir_link, sugestao_manter_link, sugestao_list_link)
+
+
+@permission_required("core.change_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_bac_manter(request):
+    """
+        Tela de Manter Sugestão de Turmas do Curso de Geografia - Bacharelado - Caicó.
+    """
+    geo_bac_ec = get_estrutura_geografia_bacharelado()
+    sugestao_incluir_link = '/core/sugestao/geo-bac/incluir'
+    sugestao_editar_link = 'sugestao_geo_bac_editar'
+    sugestao_deletar_link = 'sugestao_geo_bac_deletar'
+    sugestao_grade_link = '/core/sugestao/geo-bac'
+    return sugestao_manter(request, geo_bac_ec, sugestao_incluir_link, sugestao_grade_link,
+                           sugestao_editar_link, sugestao_deletar_link)
+
+
+@permission_required("core.add_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_bac_incluir(request):
+    geo_bac_ec = get_estrutura_geografia_bacharelado()
+    sugestao_manter_link = '/core/sugestao/geo-bac/manter'
+    return sugestao_incluir(request, geo_bac_ec, sugestao_manter_link)
+
+
+@permission_required("core.change_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_bac_editar(request, pk):
+    geo_bac_ec = get_estrutura_geografia_bacharelado()
+    return sugestao_editar(request, pk, estrutura=geo_bac_ec)
+
+
+@permission_required("core.delete_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_bac_deletar(request, pk):
+    geo_bac_ec = get_estrutura_geografia_bacharelado()
+    return sugestao_deletar(request, pk, estrutura=geo_bac_ec)
+
+
+def sugestao_geo_lic_list(request):
+    geo_lic_ec = get_estrutura_geografia_licenciatura()
+    sugestao_incluir_link = '/core/sugestao/geo-lic/incluir'
+    sugestao_manter_link = '/core/sugestao/geo-lic/manter'
+    sugestao_list_link = '/core/sugestao/geo-lic'
+    return sugestao_grade_horarios(request, geo_lic_ec, sugestao_incluir_link, sugestao_manter_link, sugestao_list_link)
+
+
+@permission_required("core.change_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_lic_manter(request):
+    """
+        Tela de Manter Sugestão de Turmas do Curso de Geografia - Licenciatura - Caicó.
+    """
+    geo_lic_ec = get_estrutura_geografia_licenciatura()
+    sugestao_incluir_link = '/core/sugestao/geo-lic/incluir'
+    sugestao_editar_link = 'sugestao_geo_lic_editar'
+    sugestao_deletar_link = 'sugestao_geo_lic_deletar'
+    sugestao_grade_link = '/core/sugestao/geo-lic'
+    return sugestao_manter(request, geo_lic_ec, sugestao_incluir_link, sugestao_grade_link,
+                           sugestao_editar_link, sugestao_deletar_link)
+
+
+@permission_required("core.add_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_lic_incluir(request):
+    geo_lic_ec = get_estrutura_geografia_licenciatura()
+    sugestao_manter_link = '/core/sugestao/geo-lic/manter'
+    return sugestao_incluir(request, geo_lic_ec, sugestao_manter_link)
+
+
+@permission_required("core.change_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_lic_editar(request, pk):
+    geo_lic_ec = get_estrutura_geografia_licenciatura()
+    return sugestao_editar(request, pk, estrutura=geo_lic_ec)
+
+
+@permission_required("core.delete_sugestaoturma", login_url='/core/usuario/logar', raise_exception=True)
+def sugestao_geo_lic_deletar(request, pk):
+    geo_lic_ec = get_estrutura_geografia_licenciatura()
+    return sugestao_deletar(request, pk, estrutura=geo_lic_ec)
