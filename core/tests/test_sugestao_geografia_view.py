84b1720df640577ce070645ade51056f7cb5114f
wanessabezerra <wanessa.bezerra.126@ufrn.edu.br>
2022-12-13 21:44:04 -0300

    :hammer: Criação do Teste de Sugestão de Geografia #256

---
+++ core/tests/test_sugestao_geografia_view.py
@@ -0,0 +1,49 @@
+import django
+django.setup()
+
+from unittest.case import TestCase
+
+from django.contrib.auth.models import User
+from django.test import Client
+from django.urls import reverse
+
+from core.models import SugestaoTurma
+from core.tests.povoar_testes import criar_dados, remover_dados
+
+class SugestaoGeografiaViewTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(SugestaoGeografiaViewTests, cls).setUpClass()
+        print('\nSugestaoGeografiaViewTests')
+        criar_dados()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        remover_dados()
+
+    def test_sugestao_detalhar(self):
+        client = Client()
+        sugestao = SugestaoTurma.objects.get(codigo_turma='01', componente__id_componente=99999)
+
+        url = reverse('sugestao_detalhar', args=(sugestao.pk,))
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_sugestao_editar(self):
+        client = Client()
+        sugestao = SugestaoTurma.objects.get(codigo_turma='01', componente__id_componente=99999)
+
+        url = reverse('sugestao_mat_editar', args=(sugestao.pk,))
+        url2 = '/core/sugestao/mat/editar/' + sugestao.pk.__str__() + '/'
+        response = client.get(url2)
+        self.assertEqual(302, response.status_code)
+
+    def test_login_success(self):
+        client = Client()
+        user = User.objects.get(username='john')
+        response = client.post('/core/usuario/logar', {'username': user.username, 'password': 'johnpassword'})
+        self.assertEqual(response.url, '/core/')
+        self.assertEqual(302, response.status_code)
