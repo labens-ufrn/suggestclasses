5e54565f3ca4999d4184ba73545611c223838a66
wanessabezerra <wanessa.bezerra.126@ufrn.edu.br>
2022-12-20 08:12:41 -0300

    :white_check_mark: Adição dos testes de turma_view.py @renatak12 #259

--- core/tests/test_turma_view.py
+++ core/tests/test_turma_view.py
@@ -46,3 +46,58 @@ class TurmaViewTests(TestCase):
 
         self.assertEqual(200, response.status_code)
 
+    def test_get_turmas_his_bac(self):
+        client = Client()
+        url = '/core/turmas/his-bac'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_geo_bac(self):
+        client = Client()
+        url = '/core/turmas/geo-bac'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_geo_lic(self):
+        client = Client()
+        url = '/core/turmas/geo-lic'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_ped(self):
+        client = Client()
+        url = '/core/turmas/ped'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_adm(self):
+        client = Client()
+        url = '/core/turmas/adm'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_let_esp(self):
+        client = Client()
+        url = '/core/turmas/let-esp'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_let_pt(self):
+        client = Client()
+        url = '/core/turmas/let-por'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
+
+    def test_get_turmas_let_ing(self):
+        client = Client()
+        url = '/core/turmas/let-ing'
+        response = client.get(url)
+
+        self.assertEqual(200, response.status_code)
